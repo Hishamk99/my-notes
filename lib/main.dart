7ec8e6bcbfd39a35b114737869e0bd64be75d9f4
hishamkame <hishamkamel370@gmail.com>
2024-04-27 13:23:30 +0300

    add (AddNoteCubit) to AddNoteButtomSheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:my_notes/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:my_notes/model/note_model.dart';
 import 'package:my_notes/screen/edit_view.dart';
 import 'package:my_notes/screen/home_page.dart';
@@ -18,18 +20,23 @@ class MyNote extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
-        useMaterial3: false,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+          useMaterial3: false,
+        ),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          NotesPage.id: (context) => const NotesPage(),
+          EditPage.id: (context) => const EditPage(),
+        },
+        initialRoute: NotesPage.id,
       ),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        NotesPage.id: (context) => const NotesPage(),
-        EditPage.id: (context) => const EditPage(),
-      },
-      initialRoute: NotesPage.id,
     );
   }
 }
--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:my_notes/cubits/add_note_cubit/add_note_cubit.dart';
 import 'add_note_form.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
@@ -7,9 +10,23 @@ class AddNoteButtomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: AddNoteForm(),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteSuccess) {
+            Navigator.pop(context);
+          }
+          if (state is AddNoteFailure) {
+            print(state.errorMessage);
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const AddNoteForm());
+        },
+      ),
     );
   }
 }
